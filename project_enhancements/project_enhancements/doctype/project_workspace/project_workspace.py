7657f1d137b25dabfe5b9966b92e3768698e1724
Nikolas Bradshaw <nikolas.bradshaw@sapphirefountains.com>
2025-11-14 10:08:12 -0700

    Create project_workspace.py

---
+++ project_enhancements/project_enhancements/doctype/project_workspace/project_workspace.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Sapphire Fountains and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class ProjectWorkspace(Document):
+    pass
